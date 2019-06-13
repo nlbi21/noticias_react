ba47eac745eba761b83903b3cbd38a06e756763a
Nelbi Alvarado <joseegnzalez@gmail.com>
2019-06-12 22:14:35 -0400

    Updates

---
+++ precache-manifest.4a76a41ade6745d7df400abba47664dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86ba66bbdd2a371b43baa1b55cffd6a4",
+    "url": "/nlbi21/noticias_react/index.html"
+  },
+  {
+    "revision": "fbd224aed31f86fa4a3f",
+    "url": "/nlbi21/noticias_react/static/css/main.ad7b9162.chunk.css"
+  },
+  {
+    "revision": "e083292f352b8a48b197",
+    "url": "/nlbi21/noticias_react/static/js/2.61ebbd48.chunk.js"
+  },
+  {
+    "revision": "fbd224aed31f86fa4a3f",
+    "url": "/nlbi21/noticias_react/static/js/main.173caf2c.chunk.js"
+  },
+  {
+    "revision": "0c4fcaab7d1e6aa2cd30",
+    "url": "/nlbi21/noticias_react/static/js/runtime~main.c22e5d29.js"
+  }
+]);
